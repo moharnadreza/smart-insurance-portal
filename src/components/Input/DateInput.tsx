f1bdb4451c9ef8c24dc770d42c229ffb48266463
Mohammadreza <moharnadreza@gmail.com>
2025-03-09 14:50:44 +0330

    feat(components): date input

---
+++ src/components/Input/DateInput.tsx
@@ -0,0 +1,42 @@
+import classNames from "classnames";
+import type { Ref } from "react";
+import type { CommonInputProps } from "./types";
+import { InputWrapper } from "./Wrapper";
+
+type Props = {
+  ref: Ref<HTMLInputElement>;
+} & CommonInputProps;
+
+const DateInput = ({
+  ref,
+  label,
+  isRequired,
+  error,
+  placeholder,
+  name,
+  isDisabled,
+}: Props) => {
+  const className = classNames(
+    [
+      "w-full ps-3 pe-10 py-2 text-sm bg-none appearance-none rounded-lg border-1 border-gray-200",
+      "transition-colors duration-50 ease-out",
+      "placeholder:text-gray-300 placeholder:text-sm focus:outline-none focus:border-gray-500",
+    ],
+    {
+      "pointer-events-none bg-gray-100": isDisabled,
+      "border-red-500 focus:border-red-500": error,
+    }
+  );
+
+  return (
+    <InputWrapper {...{ name, error, label, isDisabled, isRequired }}>
+      <input
+        type="date"
+        {...{ name, ref, placeholder }}
+        className={className}
+      />
+    </InputWrapper>
+  );
+};
+
+export { DateInput };
--- src/components/Input/index.ts
+++ src/components/Input/index.ts
@@ -1,3 +1,4 @@
 export * from "./CurrencyInput";
 export * from "./SelectInput";
 export * from "./TextInput";
+export * from "./DateInput";
