e270e4f4631ee2431fb3ec8f54cbc977d23cd466
Mohammadreza <moharnadreza@gmail.com>
2025-03-10 14:07:54 +0330

    feat(pages): submissions table loading state

--- src/app/components/InsuranceSubmissions/InsuranceSubmissions.tsx
+++ src/app/components/InsuranceSubmissions/InsuranceSubmissions.tsx
@@ -1,16 +1,19 @@
 import { useInsuranceSubmissions } from "api/hooks/useInsuranceSubmissions";
 import { Table } from "components";
+import { InsurancePlansSkeleton } from "./InsuranceSubmissionsSkeleton";
 
 const InsuranceSubmissions = () => {
   const { data: submissions, isLoading } = useInsuranceSubmissions();
-
-  if (isLoading) return "loading...";
 
   return (
     <>
       <div className="mt-16 flex justify-center flex-col gap-4">
         <h3 className="m-0 text-3xl font-bold">Submissions</h3>
-        <Table data={submissions?.data} columns={submissions?.columns} />
+        {isLoading ? (
+          <InsurancePlansSkeleton />
+        ) : (
+          <Table data={submissions?.data} columns={submissions?.columns} />
+        )}
       </div>
     </>
   );
